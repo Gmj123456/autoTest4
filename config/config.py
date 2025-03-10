c0c7e82fc51074888402c98c88bb3b87dff6bf0e
Gmj123456 <1908280383@qq.com>
2025-03-10 16:11:51 +0800

    重新提交

--- config/config.py
+++ config/config.py
@@ -1,8 +1,4 @@
 # config/config.py
-<<<<<<< HEAD
 ERP_URL = "http://192.168.150.222:3066/"
-=======
-ERP_URL = "https://your-erp-system-url.com"
->>>>>>> 2e068f1702c4e5bb4a5c82a1029000b38b83cdbc
 USERNAME = "guomj"
 PASSWORD = "qwe123"
