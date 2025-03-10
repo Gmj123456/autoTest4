5e07a5a478d07f60be682e1f9eeacddba7976644
Gmj123456 <1908280383@qq.com>
2025-03-10 16:55:56 +0800

    Auto commit

--- testcase/conftest.py
+++ testcase/conftest.py
@@ -1,15 +1,19 @@
 # testcase/conftest.py
 import time
-
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from pages.login_page import LoginPage
 from config.config import USERNAME, PASSWORD, ERP_URL
 
+# 指定 chromedriver.exe 的路径
+CHROME_DRIVER_PATH = "./utils/chromedriver.exe"
 
 @pytest.fixture(scope="function")
 def driver():
-    driver = webdriver.Chrome()
+    # 使用 Service 类指定 chromedriver.exe 的位置
+    service = Service(CHROME_DRIVER_PATH)
+    driver = webdriver.Chrome(service=service)
 
     driver.maximize_window()  # 设置全屏
     time.sleep(1)
